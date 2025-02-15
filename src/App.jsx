48d0dfebf9091b31f9dbfcb8ca8a04f5c8c3ad95
Fabio Acosta <fabioacosta00001@gmail.com>
2025-02-15 19:56:50 +0000

    La autenticación funciona pero redirige a dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [usuario, setUsuario] = useState([]);
+
+  const Consulta = async () => {
+    const response = await fetch("http://platita.test/api/usuario", { credentials: "include", });
+
+    if (response.redirected) {
+      window.location.href = response.url;
+      return;
+    }
+    const data = await response.json();
+    console.log(data);
+  }
 
   return (
     <>
@@ -20,6 +32,9 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        <button onClick={() => Consulta()}>
+          Consultar
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
