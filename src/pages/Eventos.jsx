f945ec27391ba399477a4533158ee97b10bfb466
Fabio Acosta <fabioacosta00001@gmail.com>
2025-02-16 20:18:42 +0000

    Filtrado general de eventos creado

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -17,13 +17,23 @@ export default function Eventos() {
         return eventos.filter(item => {
             const nombreFilter = searchParams.get("nombre");
             const descripcionFilter = searchParams.get("descripcion");
-            const fechaFilter = searchParams.get("fecha");
+            const tipoFilter = searchParams.get("tipo");
+            const fechaInicioFilter = searchParams.get("fecha_inicio");
+            const fechaFinFilter = searchParams.get("fecha_fin");
+            const aforoFilter = searchParams.get("aforo");
+            const accesibilidadFilter = searchParams.get("accesibilidad");
+            const estadoFilter = searchParams.get("estado");
 
             // Aplicar todos los filtros a la vez
             return (
                 (!nombreFilter || item.nombre.includes(nombreFilter)) &&
                 (!descripcionFilter || item.descripcion.includes(descripcionFilter)) &&
-                (!fechaFilter || item.fecha.includes(fechaFilter))
+                (!tipoFilter || item.tipo == tipoFilter) &&
+                (!fechaInicioFilter || item.fecha_inicio == fechaInicioFilter) &&
+                (!fechaFinFilter || item.fecha_fin == fechaFinFilter) &&
+                (!aforoFilter || item.aforo >= aforoFilter) &&
+                (!accesibilidadFilter || item.accesibilidad == accesibilidadFilter) &&
+                (!estadoFilter || item.estado == estadoFilter)
             );
         });
     }
@@ -34,12 +44,40 @@ export default function Eventos() {
 
     if (loading || !eventos) return (<h1>Buscando la dimensión adecuada...</h1>);
     if (error) return (<h1>La pistola de portales no funciona...</h1>);
+    console.log(handleFilter())
 
     return (
         <div>
             <h1>Eventos</h1>
             <input type="text" name="nombre" onChange={handleChange} alt='Buscador' value={searchParams.get("nombre") || ""} />
+
             <input type="text" name="descripcion" onChange={handleChange} alt='Buscador' value={searchParams.get("descripcion") || ""} />
+
+            <select name="tipo" onChange={handleChange} alt='Buscador' value={searchParams.get("tipo") || ""}>
+                <option value=""></option>
+                <option value="evento">evento</option>
+                <option value="actividad">actividad</option>
+            </select>
+
+            <input type="datetime-local" name="fecha_inicio" onChange={handleChange} alt='Buscador' value={searchParams.get("fecha_inicio") || ""} />
+            <input type="datetime-local" name="fecha_fin" onChange={handleChange} alt='Buscador' value={searchParams.get("fecha_fin") || ""} />
+
+            <input type="number" name="aforo" onChange={handleChange} alt='Buscador' value={searchParams.get("aforo") || ""} />
+
+            <select name="accesibilidad" onChange={handleChange} alt='Buscador' value={searchParams.get("accesibilidad") || ""}>
+                <option value=""></option>
+                <option value="socios">socios</option>
+                <option value="publico">publico</option>
+                <option value="privado">privado</option>
+                <option value="mixto">mixto</option>
+            </select>
+
+            <select name="estado" onChange={handleChange} alt='Buscador' value={searchParams.get("estado") || ""}>
+                <option value=""></option>
+                <option value="abierto">abierto</option>
+                <option value="cerrado">cerrado</option>
+            </select>
+            
             <div>
                 {handleFilter().map(evento => {
                     return <div key={evento.id}>
