88218966160150901b7debf3c666c32b7628ff20
wachipato <116815367+WACHIDADUCK@users.noreply.github.com>
2025-02-17 17:10:55 +0000

    maquetado asociaciones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <nav className="navbar">
+            <div className="logo">
+                <Link to="/" >
+                    <img className="logo" src="/img/logo.png" alt="logo" />
+                </Link>
+            </div>
+
+            <div className="search-bar">
+                <input type="text" placeholder="Buscar..."></input>
+                <button type="submit">🔍</button>
+            </div>
+
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/asociaciones">Asociaciones</Link></li>
+                <li><Link to="/eventos">Eventos</Link></li>
+            </ul>
+        </nav>
     )
 }
--- src/pages/Asociaciones.jsx
+++ src/pages/Asociaciones.jsx
@@ -1,41 +1,21 @@
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useFetch } from "../hooks/useFetch";
+import ListaAsociaciones from "../components/listaAsociaciones";
+import { Link } from "react-router-dom";
 
 export default function Asociaciones() {
-    let [searchParams, setSearchParams] = useSearchParams();
-    //https://guillermo.informaticamajada.es
-    const { data, loading, error } = useFetch("http://platita.test/api/asociacion");
-    const [asociaciones, setAsociaciones] = useState([]);
-    const [field, setField] = useState("nombre");
-
-    const handleChange = (e) => {
-        setSearchParams({ [e.target.name]: e.target.value });
-    };
-
-    const handleFilter = () => {
-        return asociaciones.filter(item => item[field].includes(searchParams.get(`${field}`) || ""))
-    }
-
-    useEffect(() => {
-        if (data) setAsociaciones(data.data);
-    }, [data]);
-
-    if (loading || !asociaciones) return (<h1>Buscando la dimensión adecuada...</h1>);
-    if (error) return (<h1>La pistola de portales no funciona...</h1>);
 
     return (
-        <div>
-            <h1>Asociaciones</h1>
-            <input type="text" name={field} onChange={handleChange} alt='Buscador' value={searchParams.get(`${field}`) || ""}></input>
-            <button onClick={() => setField("nombre")}>Nombre</button>
-            <button onClick={() => setField("descripcion")}>Descripción</button>
-            <div>
-                {handleFilter().map(asociacion => {
-                    return <div key={asociacion.id}>
-                        <h2>Nombre:{asociacion.nombre} | Descripcion:{asociacion.descripcion}</h2>
-                    </div>
-                })}
+        <div className="asociacionesContainer">
+            <div className="col-1">
+                <div className="asideDiv justify-center">
+                    <Link className="link" to="/crearAsociacion"> Crear Asociacion</Link>
+                </div>
+
+                <div className="asideDiv misAsociaciones">
+                    <h4>Mis asociaciones</h4>
+                </div>
+            </div>
+            <div className="col-2">
+            <ListaAsociaciones />
             </div>
         </div>
     )
