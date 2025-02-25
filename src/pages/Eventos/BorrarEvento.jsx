6c2741619dc3d2fbd6b5a3f6ed2fffc964d50b7b
Fabio Acosta <fabioacosta00001@gmail.com>
2025-02-25 10:55:38 +0000

    Borrar evento implementado

--- src/pages/Eventos/BorrarEvento.jsx
+++ src/pages/Eventos/BorrarEvento.jsx
@@ -1,15 +1,25 @@
 import axios from "../../hooks/axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const BorrarEvento = () => {
-    const eliminar = () => {
+    const params = useParams();
 
+    const eliminar = async () => {
+        try {
+            await axios.get("/sanctum/csrf-cookie");
+            await axios.delete(`/api/evento/${params.id}`)
+            alert("Evento eliminado correctamente");
+            window.location.href = '/eventos';
+        } catch (error) {
+            console.error("Error creando el evento:", error);
+        }
     }
+
     return (
         <>
             <h2>Estás seguro de que quieres eliminar este evento</h2>
             <div>
-                <button type="button">Si</button>
+                <button type="button" onClick={eliminar}>Si</button>
                 <button type="button">
                     <Link to="/eventos">No</Link>
                 </button>
